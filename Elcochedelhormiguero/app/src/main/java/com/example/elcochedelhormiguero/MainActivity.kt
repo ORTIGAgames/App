df3164aa7117c4606c7c93881bf5f0a94699c8a5
ORTIGAgames <jesusculebras.scf@gmail.com>
2022-11-03 19:07:14 +0100

    Material girl
    Ya esta material3 y el videotutorial

--- Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
+++ Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
@@ -4,77 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.elcochedelhormiguero.ui.theme.ElCochedelHormigueroTheme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Column
-
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.elcochedelhormiguero.ui.theme.Material3AppTheme
 
-data class Message(val author: String, val body: String)
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ElCochedelHormigueroTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                MessageCard(Message("Android", "Jetpack Compose"))
-
+            Material3AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = {}) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+                        },
+                        topBar = {
+                            SmallTopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) { values ->
+                        LazyColumn(contentPadding = values) {
+                            items(20) {
+                                ImageCard(
+                                    title = "Bacon ipsum",
+                                    description = "Bacon ipsum dolor amet pork shankle beef andouille ball tip. Meatball corned beef swine, strip steak bacon jerky doner tongue biltong pork loin drumstick sausage hamburger burgdoggen.",
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
-               }
             }
-        }
-    }
-
-
-@Preview
-@Composable
-fun PreviewMessageCard() {
-    ElCochedelHormigueroTheme {
-        Surface {
-            MessageCard(
-                msg = Message("mysingingmonster", "Take a look at Jetpack Compose, it's great!")
-
-            )
-        }
-    }
-}
-
-@Composable
-fun MessageCard(msg: com.example.elcochedelhormiguero.Message) {
-    // Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-        )
-
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Column {
-            Text(text = msg.author)
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
         }
     }
 }
