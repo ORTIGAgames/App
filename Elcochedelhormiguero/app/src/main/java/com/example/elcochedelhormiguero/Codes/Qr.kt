68e7824e9cbed255f34a5bf443b7ad61365d9390
Marcos <marcos29diciembre@gmail.com>
2022-11-05 19:04:55 +0100

    Eliminados imports innecesarios

--- Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/Codes/Qr.kt
+++ Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/Codes/Qr.kt
@@ -1,7 +1,6 @@
 package com.example.elcochedelhormiguero.Codes
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
+++ Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
@@ -1,42 +1,10 @@
 package com.example.elcochedelhormiguero
 
-import android.Manifest
-import android.content.pm.PackageManager
+
 import android.os.Bundle
-import android.util.Size
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import com.example.elcochedelhormiguero.ui.theme.Material3AppTheme
 
 @ExperimentalMaterial3Api
