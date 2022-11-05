760cd803c17a11b902ebe4d6786874632dad35b1
Marcos <marcos29diciembre@gmail.com>
2022-11-05 19:02:21 +0100

    Extra File

--- Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
+++ Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3AppTheme {
-                Qr(BarCodeQr(), "https://www.youtube.com/watch?v=TCoZoL2RFOU")
+
             }
         }
     }
