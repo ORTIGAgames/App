d3f855a25b920489bd9da1d1a5ea32c18a4370bf
Marcos <marcos29diciembre@gmail.com>
2022-11-07 18:29:14 +0100

    Changes

--- Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/QrCodeScanner.kt
+++ Elcochedelhormiguero/app/src/main/java/com/example/elcochedelhormiguero/QrCodeScanner.kt
@@ -2,11 +2,8 @@ package com.example.elcochedelhormiguero
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Bundle
 import android.util.Size
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -17,10 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -34,10 +28,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import com.example.elcochedelhormiguero.ui.theme.Material3AppTheme
 
 @Composable
 fun QrCodeScanner() {
